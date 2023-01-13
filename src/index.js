dbdfa36dd6a86afc507a8305c1f3bcb3876012bd
Doug Hauenstein <doug.hauenstein@gmail.com>
2023-01-13 14:46:33 -0500

    starting code

--- src/index.js
+++ src/index.js
@@ -5,3 +5,5 @@ before.innerHTML = JSON.stringify(companiesData, null, 2);
 
 const manipulatedData = [];
 //TODO: add the manipulated data array to the 'after' div.
+const after = document.getElementById("after");
+after.innerHTML = JSON.stringify(manipulatedData, null, 2);
