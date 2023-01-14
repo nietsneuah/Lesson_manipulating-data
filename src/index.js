f29fbcf4072ef63343f24423e90c36a73ee4da06
Doug Hauenstein <doug.hauenstein@gmail.com>
2023-01-14 12:29:21 -0500

    added add elements methods

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { companiesData } from "./companies-data";
-
+import { extraRecord } from "./extra-record";
 // before and after are divs in the index.html file
 const before = document.getElementById("before");
 // TODO: manipulate the data array and store it in a new variable
@@ -9,6 +9,10 @@ const before = document.getElementById("before");
 // const manipulatedData = companiesData.pop();
 // shift the first element from the array
 // const manipulatedData = companiesData.shift();
+// push the extraRecord to the end of the array
+// const manipulatedData = companiesData.push(extraRecord);
+// unshift the extraRecord to the beginning of the array
+const manipulatedData = companiesData.unshift(extraRecord);
 
 //TODO: add the manipulated data array to the 'after' div.
 before.innerHTML = JSON.stringify(companiesData, null, 2);
