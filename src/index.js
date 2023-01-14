af4937f3082aa1ef0f35d3ac25f534f994cc0a45
Doug Hauenstein <doug.hauenstein@gmail.com>
2023-01-14 14:55:31 -0500

    static Filter

--- src/index.js
+++ src/index.js
@@ -12,7 +12,9 @@ const before = document.getElementById("before");
 // push the extraRecord to the end of the array
 // const manipulatedData = companiesData.push(extraRecord);
 // unshift the extraRecord to the beginning of the array
-const manipulatedData = companiesData.unshift(extraRecord);
+// const manipulatedData = companiesData.unshift(extraRecord);
+// filter the array to only include state of "TX"
+const manipulatedData = companiesData.filter((company) => company.fieldData.State === "TX" || company.fieldData.State === "PA");
 
 //TODO: add the manipulated data array to the 'after' div.
 before.innerHTML = JSON.stringify(companiesData, null, 2);
@@ -20,3 +22,5 @@ const after = document.getElementById("after");
 after.innerHTML = JSON.stringify(manipulatedData, null, 2);
 // console log companiesData Length
 console.log(companiesData.length);
+// console log manipulatedData Length
+console.log(manipulatedData.length);
