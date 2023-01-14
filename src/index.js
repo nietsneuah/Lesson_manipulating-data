5921cbbb7d615e0f57b1dc27c1456a94243430f8
Doug Hauenstein <doug.hauenstein@gmail.com>
2023-01-14 12:18:44 -0500

    Added Pop and Shift

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import { companiesData } from "./companies-data";
 
+// before and after are divs in the index.html file
 const before = document.getElementById("before");
-before.innerHTML = JSON.stringify(companiesData, null, 2);
+// TODO: manipulate the data array and store it in a new variable
+// const manipulatedData = [companiesData[0]];
 
-const manipulatedData = [];
+// pop the last element from the array
+// const manipulatedData = companiesData.pop();
+// shift the first element from the array
+// const manipulatedData = companiesData.shift();
+
 //TODO: add the manipulated data array to the 'after' div.
+before.innerHTML = JSON.stringify(companiesData, null, 2);
 const after = document.getElementById("after");
 after.innerHTML = JSON.stringify(manipulatedData, null, 2);
+// console log companiesData Length
+console.log(companiesData.length);
